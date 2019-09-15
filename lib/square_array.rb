aa516eb606756cabec17c2fddd7f2625f6d11416
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-15 06:27:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,29 @@
+def output_array_elements(array)
+ new_numbers = []
+ counter = 0
+ while counter < array.length do
+   new_numbers << array[counter]**2 
+   counter += 1
+   
+ end
+ return new_numbers
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def square_array(array)
  counter = 0
  
@@ -6,3 +32,4 @@ def square_array(array)
    counter += 1
  end
 end
+=end
